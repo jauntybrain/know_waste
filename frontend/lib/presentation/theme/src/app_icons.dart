f5c2cb7b7be8983f9873b59eedcf0eebc7732f92
Pavel <pavel2002@ryabov.co>
2023-08-14 20:41:23 -0700

    refactor: rework assets

---
+++ frontend/lib/presentation/theme/src/app_icons.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class AppIcons {
+  static const iconsPath = 'assets/icons';
+
+  /// Define local assets
+  static const String logoHorizontal = 'logo-horizontal.svg';
+  static const String wasteScanner = 'waste-scanner.svg';
+  static const String comingSoon = 'coming-soon.svg';
+  static const String advice = 'advice.svg';
+  static const String tips = 'tips.svg';
+  static const String local = 'local.svg';
+
+  /// Check if the icon is vector
+  static bool isSvg(String path) => path.contains('svg');
+
+  /// Return the icon
+  static Widget icon(
+    String path, {
+    double? size,
+    Color? color,
+  }) {
+    if (isSvg(path)) {
+      return SvgPicture.asset(
+        '$iconsPath/$path',
+        width: size,
+        height: size,
+        color: color,
+      );
+    } else {
+      return Image.asset(
+        '$iconsPath/$path',
+        width: size,
+        height: size,
+      );
+    }
+  }
+}
