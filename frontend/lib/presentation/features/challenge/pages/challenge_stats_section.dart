1315948e88075784cf61c682c39b2fd990cdf133
Pavel <pavel2002@ryabov.co>
2023-08-16 04:04:17 -0700

    cleanup

--- frontend/lib/presentation/features/challenge/pages/challenge_stats_section.dart
+++ frontend/lib/presentation/features/challenge/pages/challenge_stats_section.dart
@@ -8,6 +8,7 @@ class ChallengeStatsSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           'Challenge Stats',
